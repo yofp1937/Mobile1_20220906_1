a56630c441ba36bbe0c8e6f31c91f87a2550fa79
yofp1937 <112924147+yofp1937@users.noreply.github.com>
2022-09-06 14:34:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mobile1_20220906_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile1_20220906_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
